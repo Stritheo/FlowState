c9f37fa553ab774b79938aea72927138c7b27108
YOUR_GITHUB_USERNAME <your.email@example.com>
2025-08-31 09:05:08 +1000

    Add Diamond Clarity branding assets and documentation

--- hooks/useColorScheme.ts
+++
@@ -1,6 +0,0 @@
-import { useColorScheme as _useColorScheme } from 'react-native';
-
-// This hook returns 'light' | 'dark' | null
-export default function useColorScheme() {
-	return _useColorScheme();
-}
